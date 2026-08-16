d553009c7a1e1011d631b0a125d78aafcb69f216
D'MARV.. <marvennis08@gmail.com>
2026-08-16 19:46:34 +0100

    Describe your changes

--- Intro.js
+++ Intro.js
@@ -1,1 +1,8 @@
-console.log("Marvellous is awesome!")
+Name = "Marvellous Ennis";
+Age = 18;
+FavoriteLanguage = "JavaScript";
+
+console.log("Hello, my name is " + Name + ". I am " + Age + " years old and my favorite programming language is " + FavoriteLanguage + ".");
+console.log("I am excited to learn more about programming and improve my skills in " + FavoriteLanguage + "!");
+console.log("I hope to build amazing projects and contribute to the tech community in the future.");
+console.log("Thank you for taking the time to read my introduction. Let's code and create something awesome together!");
